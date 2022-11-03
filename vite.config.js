f5ca772ca4b8167b0ba5155a5de01ee199b84c16
Luiz Felipe <felipefortescastro@gmail.com>
2022-11-02 22:35:48 -0300

    tried to fix bugs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default {
+    plugins: [
+      react(),
+        viteCommonjs()
+    ],
+    build:{
+      rollupOptions:{
+        external:['node_modules/styled-components/dist/styled-components.browser.esm.js'],
+        outDir:'build',
+      }
+    }
+    //node_modules/styled-components/dist/styled-components.browser.esm.js
+}
+
+
