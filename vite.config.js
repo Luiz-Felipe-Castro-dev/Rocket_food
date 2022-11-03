55bbe9c8c0c23192ffa1bf67366b5ef589498f6c
Luiz Felipe <felipefortescastro@gmail.com>
2022-11-03 00:39:31 -0300

    .......................

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+// import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+import { fileURLToPath } from 'node:url'
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [react(), viteCommonjs()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react-is',
-        fileURLToPath(
-          new URL('styled-components/dist/styled-components.browser.esm.js'),
-          /node_modules/
-        )
-      ]
-    }
-  }
+  plugins: [react()],
+  // build: {
+  //   rollupOptions: {
+  //     external: [
+  //       'react-is',
+  //       fileURLToPath(
+  //         new URL('styled-components/dist/styled-components.browser.esm.js'),
+  //         /node_modules/
+  //       )
+  //     ]
+  //   }
+  // }
   //node_modules/styled-components/dist/styled-components.browser.esm.js
 }
